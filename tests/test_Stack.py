508f23f3545898a7c7e456e32bcaa1112b87e3f2
kirsansan <kirill_home@mail.ru>
2023-04-16 01:28:22 +0300

    add tests for pop() method for Stack

--- tests/test_Stack.py
+++ tests/test_Stack.py
@@ -4,7 +4,7 @@ from datatypes.node import Node
 from datatypes.stack import Stack
 
 class StackTest(TestCase):
-    def test_stack(self):
+    def test_stack_init_and_push(self):
         stack = Stack()
         stack.push("i'am first element")
         stack.push("i'am second element")
@@ -19,4 +19,14 @@ class StackTest(TestCase):
         stack.push({"name": "foo", 5: "bar"})
         self.assertEqual(stack.top.data, {5: 'bar', 'name': 'foo'})
 
+    def test_stack_pop(self):
+        stack = Stack()
+        stack.push("i'am first element")
+        stack.push("i'am second element")
+        stack.push("i'am third element")
+        self.assertEqual(stack.pop(), "i'am third element")
+        self.assertEqual(stack.pop(), "i'am second element")
+        self.assertEqual(stack.pop(), "i'am first element")
+        self.assertEqual(stack.pop(), None)
+        self.assertEqual(stack.pop(), None)
 
