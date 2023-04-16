0562c848c41e063ff15132f04d182ce9e2021006
kirsansan <kirill_home@mail.ru>
2023-04-16 16:59:49 +0300

    make attractive view of main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datatypes.linkedlist import LinkedList
 
 if __name__ == '__main__':
 
+# node types
     n1 = Node(5, None)
     n2 = Node('a', n1)
     print(n1.data)
@@ -13,6 +14,7 @@ if __name__ == '__main__':
     print(n2)
     print(n2.next_node, "\n")
 
+# stack types
     stack = Stack()
     stack.push('data1')
     stack.push('data2')
@@ -22,10 +24,11 @@ if __name__ == '__main__':
     print(stack.top.next_node.next_node.data)
     print(stack.top.next_node.next_node.next_node)
     #print(stack.top.next_node.next_node.next_node.data)
-
     for _ in range(0, 4):
         print("pop result is ", stack.pop())
+    print("\n")
 
+# queue types
     queue = Queue()
     queue.add('queue1')
     queue.add('queue2')
@@ -33,10 +36,12 @@ if __name__ == '__main__':
     print("current length of queue is ", len(queue))
     for _ in range(0, 4):
         print("remove result is ", queue.remove())
+    print("\n")
 
+# linked list types
     llist = LinkedList()
     llist.add_to_tail('llist1')
     llist.add_to_tail({"name": "somedata2", "value": "somevalue2"})
     llist.add_to_head('llist0')
     print(llist)
-    print(len(llist))
+    print("current length of linked_list is ", len(llist))
